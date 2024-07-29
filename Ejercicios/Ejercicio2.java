4c8da5f25f858e6dd4c50136fd094c5d8b2a34ae
karinatabares1 <marianaocampomedina@gmail.com>
2024-07-28 20:04:23 -0500

    Estructuras iterativas For

---
+++ Ejercicios/Ejercicio2.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		   int numero = 3;
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(numero + " x " + i + " = " + (numero * i));
+	        }
+
+	}
+
+}
